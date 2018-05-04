83ed9f06b9e8b26306d8f7ea615555694f8683e3
w407477841 <407477841@qq.com>
2018-05-04 16:01:38 +0800

    v1.102

--- src/main/resources/sql/VideoConfigRights.sql
+++
@@ -1,6 +0,0 @@
-insert into `t_right` (right_name,module_id,available_devices,url,right_txt) values ('videoConfig_add',18,'111','/videoConfig','新增');
-insert into `t_right` (right_name,module_id,available_devices,url,right_txt) values ('videoConfig_deleted',18,'111','/videoConfig','删除');
-insert into `t_right` (right_name,module_id,available_devices,url,right_txt) values ('videoConfig_update',18,'111','/videoConfig','修改');
-insert into `t_right` (right_name,module_id,available_devices,url,right_txt) values ('videoConfig_info',18,'111','/videoConfig','信息');
-insert into `t_right` (right_name,module_id,available_devices,url,right_txt) values ('videoConfig_page',18,'111','/videoConfig/page/','分页');
-insert into `t_right` (right_name,module_id,available_devices,url,right_txt) values ('videoConfig_all',18,'111','/videoConfig','查询');
